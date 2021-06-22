7efef90d506ca0647eeaf6467e979336d608d8fd
Cameron39 <cameron.39@gmail.com>
2021-06-21 21:26:53 -0500

    Answers List. Ready to compare user answer with actual answer

--- lib/main.dart
+++ lib/main.dart
@@ -27,9 +27,16 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [
-    Icon(Icons.check, color: Colors.green),
-    Icon(Icons.close, color: Colors.red)
+    //Icon(Icons.check, color: Colors.green),
+    //Icon(Icons.close, color: Colors.red)
   ]; //brackets are for lists/arrays
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+  int qNum = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[qNum],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -67,7 +74,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  qNum++;
+                });
               },
             ),
           ),
@@ -85,7 +94,9 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  qNum++;
+                });
               },
             ),
           ),
